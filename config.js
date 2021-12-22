9706bf13755fa4b0f5cf55af9b7a3c1a6c894b65
byhim27 <95236075+byhim27@users.noreply.github.com>
2021-12-22 22:34:29 +0700

    him

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee loo cậu 😍',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Đồng Ý',
+    title: 'Cậu thích tớ có phải không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu đồng ý làm ng iu tớ đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'khônggg nha ',
+    question:'Nói cho tới lý do vì sao cậu thích tớ đii <3',
+    btnReply: 'Gửi cho tớ 😘',
+    reply: 'Tại vì cậu đẹp trai <3',
+    mess: 'Tớ biết mà 🥰. Tớ cũng thích cậu  😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không nhắn tin cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100025247501919'
+}
